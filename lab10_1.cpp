33da6c6eb3267851e6f5e7d9a3d4de74500a1e2b
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2022-12-27 15:51:17 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main(){	
+	double loan,rate,pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin >> pay;
+	
+	double pre,in,total,N=0;
+	int year =1;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +20,47 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+	pre=loan;
+	in=pre*(rate/100);
+	total=pre+in;
+	N=total-pay;
+	if(total<=pay){
+		pay =total;
+		N = 0;
+			
+		
+	}
+		
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << pre;
+	cout << setw(13) << left << in;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << pay;
+	cout << setw(13) << left << N;
 	cout << "\n";	
+    year =2;
+	while(N!=0){
+	    
+		pre=N;
+		in=pre*(rate/100);
+		total=pre+in;
+		N=total-pay;
+		if(N<=0){
+			pay=total;
+			N=0;
+			
+		}
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << pre;
+		cout << setw(13) << left << in;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << N;
+		cout << "\n";	
 	
-	return 0;
-}
+		year++;
+	
+	}
+}	
